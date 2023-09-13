b8d42526dec7ebe5b95e02625553f46dd6cfb170
silva <silvakeci.ing@gmail.com>
2023-09-14 00:33:45 +0300

    form input component, button component

--- src/Component/form-input/form-input.jsx
+++ src/Component/form-input/form-input.jsx
@@ -1,8 +1,18 @@
+import "./form-input.scss";
+
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input {...otherProps} />
+    <div className="group">
+      <input className="form-input" {...otherProps} />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          }form-input-label`}
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 };
--- src/Component/sign-up/sign-up.jsx
+++ src/Component/sign-up/sign-up.jsx
@@ -3,6 +3,9 @@ import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/fireBase";
+import FormInput from "../form-input/form-input";
+import "./sign-up.scss";
+import Button from "../button/button";
 
 const defaultFormFields = {
   displayName: "",
@@ -49,38 +52,36 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h1>Sign Up with email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account</h2>
+      <span>Sign Up with email and password</span>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={handleChange}
           name="displayName"
           value={displayName}
         />
-
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           required
           onChange={handleChange}
           name="email"
           value={email}
         />
-
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
           name="password"
           value={password}
         />
-
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
@@ -88,7 +89,7 @@ const SignUp = () => {
           value={confirmPassword}
         />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
