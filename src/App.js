c9dba9ab89752f6580a9d84f3ccf96550806d16f
silvakeci1 <silvakeci.ing@gmail.com>
2023-09-15 17:15:53 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from "./routes/home/homeComponent";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import Authentication from "./routes/authentication/authentication";
+import Shop from "./routes/shop/shop";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
