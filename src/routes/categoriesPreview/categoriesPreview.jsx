876828e6ff4e8dc0835ad465e0fed95093c025a1
silva <silvakeci.ing@gmail.com>
2023-09-27 21:54:34 +0300

    changes

--- src/routes/categoriesPreview/categoriesPreview.jsx
+++ src/routes/categoriesPreview/categoriesPreview.jsx
@@ -1,26 +1,20 @@
-import {Fragment, useContext} from "react";
+import { Fragment, useContext } from "react";
 import { CategoriesContext } from "../../contexts/categoriesContext";
 import CategoryPreview from "../../Component/categoryPreview/categoryPreview";
-import {Fa0} from "react-icons/fa6";
 
 const CategoriesPreview = () => {
-    const {categoriesMap  } = useContext(CategoriesContext);
+  const { categoriesMap } = useContext(CategoriesContext);
 
-
-    return (
+  return (
     <Fragment>
-        {
-            Object.keys(categoriesMap).map(title => {
-                const products = categoriesMap[title] ;
-                return (
-                    <CategoryPreview key={title} title={title} products={products} />
-                )
-            })}
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
     </Fragment>
-
-
-
-    );
+  );
 };
 
 export default CategoriesPreview;
